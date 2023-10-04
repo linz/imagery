6a538f382a5167e5d08a0fe510806f3ab8ea0988
Paul Fouquet <pfouquet@linz.govt.nz>
2023-10-05 08:19:51 +1300

    revert: "feat: publish README.md as index.html to S3 TDE-862 (#299)"
    This reverts commit 21b319358894adc89058f4c2a7abea71f5b18f7c.

--- site/get.assets.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp node_modules/@linzjs/lui/dist/lui.css ./
-cp node_modules/@linzjs/lui/dist/assets/images/nz-govt-logo-rev.svg ./
-cp node_modules/@linzjs/lui/dist/assets/images/linz-logo-col-n-white-txt.svg ./
